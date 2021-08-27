3fc1fc4630ee4caa82c2fff45c4c25caa80b44b9
Shivangi Sharma <sharmashivangi097@gmail.com>
2021-08-27 16:56:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const billAmount = document.querySelector("#billAmount")
-const userAmount = document.querySelector('#userAmount')
-const nextBtn = document.querySelector('.next-btn')
-const checkBtn = document.querySelector('.check-btn')
-const errorMsg = document.querySelector('.errorMsg')
-const noOfNotes = document.querySelectorAll('.no-of-notes')
-const pHide = document.querySelector('.p-hide')
-const hide = document.querySelector('.hide')
-hide.style.display = "none"
+const billsAmount = document.querySelector("#billsAmount");
+const enteredAmount = document.querySelector('#enteredAmount');
+const nextBtn = document.querySelector('.next-btn');
+const checkBtn = document.querySelector('.check-btn');
+const errorMsg = document.querySelector('.errorMsg');
+const noOfNotes = document.querySelectorAll('.no-of-notes');
+const hideP = document.querySelector('.hide-p');
+const hide = document.querySelector('.hide');
+hide.style.display = "none";
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 
-checkBtn.addEventListener('click', checkBillAndUserAmount)
 
 function checkBillAndUserAmount(){
-    if (billAmount.value > 0) {
+    if (billsAmount.value > 0) {
         errorMsg.style.display = "none"
         checkUserAndBillAmount()
     }
     else {
-        showErrorMsg("Your bill amount is less than zero, please check")
+        showErrorMsg("Your bill amount is less than zero, please check!")
     }
-}
+};
 
 function checkUserAndBillAmount() {
-    if (Number(userAmount.value) >= Number(billAmount.value)) {
+    if (Number(enteredAmount.value) >= Number(billsAmount.value)) {
         // need to calculate the change
-        const amountToBeReturned = userAmount.value - billAmount.value
+        const amountToBeReturned = enteredAmount.value - billsAmount.value
         calculateChange(amountToBeReturned)
     }
     else {
-        showErrorMsg("Cash given should be greater or equal to the bill amount, please check")
+        showErrorMsg("Cash given should be greater or equal to the bill amount, please check!")
     }
-}
+};
+
+
 
 function calculateChange(amountToBeReturned) {
     for(let i = 0; i < availableNotes.length; i++){
@@ -40,22 +41,25 @@ function calculateChange(amountToBeReturned) {
         amountToBeReturned %= availableNotes[i]
         noOfNotes[i].innerText = numberofNotes;
     }
-}
+};
+
+
+
+nextBtn.addEventListener('click', () => {
+    const amount = billsAmount.value
+    if (amount >= 1) {
+        hide.style.display = "block" 
+        hideP.innerText = ""
+    }
+    else{
+        hideP.innerText = "Invalid Amount, please check!"
+    }
+});
+
+checkBtn.addEventListener('click', checkBillAndUserAmount);
+
 
 function showErrorMsg(message){
     errorMsg.style.display = "block"
     errorMsg.innerText = message
-}
-
-nextBtn.addEventListener('click', () => {
-    const amount = billAmount.value
-    if (amount >= 1) {
-        hide.style.display = "block" 
-        pHide.innerText = ""
-    }
-    else{
-        pHide.innerText = "Invalid Amount, please check"
-    }
-})
-
-
+};
